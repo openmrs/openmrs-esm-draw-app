bee07705ec9aabd15d90a2ece538b418da3bef1b
Dennis Kigen <kigen.work@gmail.com>
2023-07-07 13:05:01 +0300

    (chore) Adapt template to match requirements

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,27 @@
-module.exports = require("openmrs/default-webpack-config");
+const config = require("openmrs/default-webpack-config");
+const webpack = require("webpack");
+
+config.additionalConfig.resolve = {
+  fallback: {
+    path: require.resolve("path-browserify"),
+  },
+};
+
+config.additionalConfig.module = {
+  rules: [
+    {
+      test: /node_modules\/vfile\/core\.js/,
+      use: [
+        {
+          loader: "imports-loader",
+          options: {
+            type: "commonjs",
+            imports: ["single process/browser process"],
+          },
+        },
+      ],
+    },
+  ],
+};
+
+module.exports = config;
