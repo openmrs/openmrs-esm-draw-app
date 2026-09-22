60527ae409f027a92e3d6a4076a84e0dc1f7840f
Kavinda Weerasinghe <kavindamadhuranga74@gmail.com.com>
2026-09-22 13:21:27 +0530

    chore: update mock for react-i18next and translations

--- __mocks__/react-i18next.js
+++ __mocks__/react-i18next.js
@@ -1,10 +1,39 @@
+const React = require('react');
+
+const hasChildren = (node) => node && (node.children || (node.props && node.props.children));
+
+const getChildren = (node) => (node && node.children ? node.children : node.props && node.props.children);
+
+const renderNodes = (reactNodes) => {
+  if (typeof reactNodes === 'string') {
+    return reactNodes;
+  }
+
+  return Object.keys(reactNodes).map((key, i) => {
+    const child = reactNodes[key];
+    const isElement = React.isValidElement(child);
+
+    if (typeof child === 'string') {
+      return child;
+    }
+    if (hasChildren(child)) {
+      const inner = renderNodes(getChildren(child));
+      return React.cloneElement(child, { ...child.props, key: i }, inner);
+    }
+    if (typeof child === 'object' && !isElement) {
+      return Object.keys(child).reduce((str, childKey) => `${str}${child[childKey]}`, '');
+    }
+
+    return child;
+  });
+};
+
 module.exports = {
   useTranslation: () => ({
-    t: (key, defaultValue) => defaultValue || key,
+    t: (key, options) => (typeof options === 'string' ? options : options?.defaultValue) ?? key,
     i18n: {
       changeLanguage: () => new Promise(() => {}),
     },
   }),
-  Trans: ({ children }) => children,
+  Trans: ({ children }) => React.createElement(React.Fragment, null, renderNodes(children)),
 };
-
